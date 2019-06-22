6b1656212150e9ecf260c6a4a659f8def0737b88
NataliaColome <nmcolome@gmail.com>
2019-06-21 20:44:13 -0600

    Add Runner, start refactoring PageGenerator, create Layout

--- page_generator.rb
+++ page_generator.rb
@@ -1,19 +1,28 @@
 require 'erb'
-require './lib/stat_tracker'
+require './lib/runner'
+require 'pry'
+# require './lib/stat_tracker'
 
-game_path = './data/game.csv'
-team_path = './data/team_info.csv'
-game_teams_path = './data/game_teams_stats.csv'
+# game_path = './data/game.csv'
+# team_path = './data/team_info.csv'
+# game_teams_path = './data/game_teams_stats.csv'
 
-locations = {
-  games: game_path,
-  teams: team_path,
-  game_teams: game_teams_path
-}
+# locations = {
+#   games: game_path,
+#   teams: team_path,
+#   game_teams: game_teams_path
+# }
 
-@stat_tracker = StatTracker.from_csv(locations)
+# @stat_tracker = StatTracker.from_csv(locations)
 
+@stat_tracker = Runner.new.start
+binding.pry
 template = File.open("./site/index.erb").read
 erb = ERB.new(template, nil, '>')
 a = File.new('./site/index.html', 'w+')
 a.puts(erb.result(binding))
+
+class PageGenerator
+  
+end
+
