cc734a0144dee165621fd6d0e58a639ba1e65b89
NataliaColome <nmcolome@gmail.com>
2019-06-23 17:54:54 -0600

    tests & logic for #teams: gets team names and ids

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -43,4 +43,11 @@ RSpec.describe Team do
 
     expect(@team.team_info('1')).to eq result
   end
+
+  it '#teams' do
+  expect(@team.teams).to be_an_instance_of Array
+  expect(@team.teams.count).to eq 33
+  expect(@team.teams[0]).to eq ['Anaheim', '24']
+  expect(@team.teams[32]).to eq ['Winnipeg', '52']
+  end
 end
