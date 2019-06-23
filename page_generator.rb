fa6f31e172c2613823ec4ea51ec74aa9ce91ff30
NataliaColome <nmcolome@gmail.com>
2019-06-22 22:24:38 -0600

    Refactor PageGenerator, add CSS to layout

--- page_generator.rb
+++ page_generator.rb
@@ -1,28 +1,25 @@
 require 'erb'
 require './lib/runner'
-require 'pry'
-# require './lib/stat_tracker'
-
-# game_path = './data/game.csv'
-# team_path = './data/team_info.csv'
-# game_teams_path = './data/game_teams_stats.csv'
-
-# locations = {
-#   games: game_path,
-#   teams: team_path,
-#   game_teams: game_teams_path
-# }
-
-# @stat_tracker = StatTracker.from_csv(locations)
-
-@stat_tracker = Runner.new.start
-binding.pry
-template = File.open("./site/index.erb").read
-erb = ERB.new(template, nil, '>')
-a = File.new('./site/index.html', 'w+')
-a.puts(erb.result(binding))
 
 class PageGenerator
-  
+  def initialize
+    @stat_tracker = Runner.new.start
+    @layout = File.read('./site/layout.erb')
+    @template = File.read('./site/index.erb')
+  end
+
+  def render
+    templates = [@template, @layout]
+    templates.inject(nil) do | prev, temp |
+      _render(temp) { prev }
+    end
+  end
+
+  def _render(temp)
+    ERB.new(temp, nil, '>').result(binding)
+  end
 end
 
+output = PageGenerator.new
+a = File.new('./site/index.html', 'w+')
+a.puts(output.render)
